c27d4c5c6d4fde1446af2ef095676114f8528e24
Aayush Goyal <goyalyo203@gmail.com>
2025-03-25 10:25:48 +0530

    Fixed unused parameter in ApiKeyModal

--- app/components/ApiKeyModal.tsx
+++ app/components/ApiKeyModal.tsx
@@ -7,7 +7,7 @@ interface ApiKeyModalProps {
 }
 
 // This component is now unused since we're using a hardcoded API key
-export default function ApiKeyModal(_props: ApiKeyModalProps) {
+export default function ApiKeyModal() {
   // Component is disabled since we're using a hardcoded API key
   return null;
 } 
